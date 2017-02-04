8b04e1cf80f66af09d15a2072ef2e4ffb7687622
Rodrigo <romacas@hotmail.co.uk>
2017-02-04 13:06:51 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+var questions = [new Question("In which country is London located?", ["Scotland", "England"], "England"), 
+                 new Question("In which country is New York located?", ["England","United States"], "United States")];
+				 
+var quiz = new Quiz(questions);
+
+QuizUI.displayNext();
